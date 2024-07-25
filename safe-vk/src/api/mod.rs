42252d92a27eee6105ff00517e8cab85c6508ebd
Azumi4han <chifmaster113@gmail.com>
2024-07-25 20:08:33 +0300

    Refactor chained macros to remove boilerplate and improve readability

--- safe-vk/src/api/mod.rs
+++ safe-vk/src/api/mod.rs
@@ -74,52 +74,27 @@ impl WriteQuery for bool {
     }
 }
 
-////TODO: Macro
-impl WriteQuery for &[i64] {
-    fn write_query<W>(&self, out: &mut W)
-    where
-        W: Write + ?Sized,
-    {
-        let ids = self
-            .iter()
-            .map(|id| id.to_string())
-            .collect::<Vec<_>>()
-            .join(",");
-
-        out.write(ids.as_bytes())
-    }
+macro_rules! write_query_slice {
+    ($ty:ty) => {
+        impl WriteQuery for $ty {
+            fn write_query<W>(&self, out: &mut W)
+            where
+                W: Write + ?Sized,
+            {
+                let ids = self
+                    .iter()
+                    .map(|id| id.to_string())
+                    .collect::<Vec<_>>()
+                    .join(",");
+                out.write(ids.as_bytes())
+            }
+        }
+    };
 }
 
-impl WriteQuery for &[i32] {
-    fn write_query<W>(&self, out: &mut W)
-    where
-        W: Write + ?Sized,
-    {
-        let ids = self
-            .iter()
-            .map(|id| id.to_string())
-            .collect::<Vec<_>>()
-            .join(",");
-
-        out.write(ids.as_bytes())
-    }
-}
-
-impl WriteQuery for &[u32] {
-    fn write_query<W>(&self, out: &mut W)
-    where
-        W: Write + ?Sized,
-    {
-        let ids = self
-            .iter()
-            .map(|id| id.to_string())
-            .collect::<Vec<_>>()
-            .join(",");
-
-        out.write(ids.as_bytes())
-    }
-}
-////
+write_query_slice!(&[u32]);
+write_query_slice!(&[i32]);
+write_query_slice!(&[i64]);
 
 macro_rules! write_query {
     ($ty:ty) => {
@@ -156,6 +131,16 @@ macro_rules! api_func {
 #[macro_export]
 macro_rules! chained_method_fn {
     ($name:ident, Option<$output:ty>, $method:expr, $($fn_name:ident($ty:ty)),*) => {
+        chained_method_fn!(@common $name, Option<$output>, $method, $($fn_name($ty)),*);
+        chained_method_fn!(@into_future $name, Option<$output>, $method);
+    };
+
+    ($name:ident, $output:ty, $method:expr, $($fn_name:ident($ty:ty)),*) => {
+        chained_method_fn!(@common $name, $output, $method, $($fn_name($ty)),*);
+        chained_method_fn!(@into_future $name, $output, $method);
+    };
+
+    (@common $name:ident, $output:ty, $method:expr, $($fn_name:ident($ty:ty)),*) => {
         #[derive(Clone)]
         pub struct $name {
             request: Arc<RequestBuilder>,
@@ -169,20 +154,27 @@ macro_rules! chained_method_fn {
 
             fn write_fmt(&mut self, arg: impl fmt::Display) {
                 use std::io::Write;
-                write!(self.query, "{arg}").unwrap();
+                write!(self.query, "{}", arg).unwrap();
             }
         }
 
         impl $name {
             pub fn new(request: Arc<RequestBuilder>, peer_id: Option<i64>) -> Self {
-                let mut method = $name { request, query: Vec::new() };
-                if let Some(peer_id) = peer_id { method.arg("peer_id", peer_id); };
+                let mut method = $name {
+                    request,
+                    query: Vec::new(),
+                };
+                if let Some(peer_id) = peer_id {
+                    method.arg("peer_id", peer_id);
+                }
                 method
             }
 
-            $(api_func!($fn_name, $ty);)*
+             $(api_func!($fn_name, $ty);)*
         }
+    };
 
+    (@into_future $name:ident, Option<$output:ty>, $method:expr) => {
         impl IntoFuture for $name {
             type Output = Result<Option<$output>>;
             type IntoFuture = futures_core::future::BoxFuture<'static, Result<Option<$output>>>;
@@ -196,33 +188,8 @@ macro_rules! chained_method_fn {
             }
         }
     };
-    ($name:ident, $output:ty, $method:expr, $($fn_name:ident($ty:ty)),*) => {
-        pub struct $name {
-            request: Arc<RequestBuilder>,
-            query: Vec<u8>,
-        }
 
-        impl Write for $name {
-            fn write(&mut self, arg: &[u8]) {
-                self.query.extend_from_slice(arg);
-            }
-
-            fn write_fmt(&mut self, arg: impl fmt::Display) {
-                use std::io::Write;
-                write!(self.query, "{arg}").unwrap();
-            }
-        }
-
-        impl $name {
-            pub fn new(request: Arc<RequestBuilder>, peer_id: Option<i64>) -> Self {
-                let mut method = $name { request, query: Vec::new() };
-                if let Some(peer_id) = peer_id { method.arg("peer_id", peer_id); };
-                method
-            }
-
-            $(api_func!($fn_name, $ty);)*
-        }
-
+    (@into_future $name:ident, $output:ty, $method:expr) => {
         impl IntoFuture for $name {
             type Output = Result<$output>;
             type IntoFuture = futures_core::future::BoxFuture<'static, Result<$output>>;
